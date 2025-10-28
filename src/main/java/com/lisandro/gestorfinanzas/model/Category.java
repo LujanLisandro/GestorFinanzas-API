0b81af67cb033f5fce22d75c402debd9a1d4622f
Lisandro <lujanlisandro.11@gmail.com>
2025-10-28 08:32:49 -0300

    Remove unused imports from Category model

--- src/main/java/com/lisandro/gestorfinanzas/model/Category.java
+++ src/main/java/com/lisandro/gestorfinanzas/model/Category.java
@@ -1,19 +1,13 @@
 package com.lisandro.gestorfinanzas.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
