acf942c8b47e237f0c48be2b7a2454811d687918
Lisandro <lujanlisandro.11@gmail.com>
2026-01-06 15:57:23 -0300

    Enhance JwtUtilsTest by adding mock for DecodedJWT and setting up test for username extraction

--- src/test/java/com/lisandro/gestorfinanzas/utils/JwtUtilsTest.java
+++ src/test/java/com/lisandro/gestorfinanzas/utils/JwtUtilsTest.java
@@ -3,7 +3,10 @@ package com.lisandro.gestorfinanzas.utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 @ExtendWith(SpringExtension.class)
 public class JwtUtilsTest {
@@ -12,10 +15,13 @@ public class JwtUtilsTest {
     @BeforeEach
     void setup(){
         jwtUtils = new JwtUtils();
+
         
     }
     @Test
     void testExtractUsername(){
-
+        DecodedJWT mockDecodedJWT = Mockito.mock(DecodedJWT.class);
+        Mockito.when(mockDecodedJWT.getSubject()).thenReturn("Lisandro");
+        
     }
 }
